99c2c24e27b3caee75e77590848d9c50691705be
Tomi Bagus Nugroho <bagus11nugroho@gmail.com>
2023-05-17 21:37:27 +0700

    change firebase cdn

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js'
 
-    // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-    import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-analytics.js'
-
-    // Add Firebase products that you want to use
-    import { getAuth } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js'
-    import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js'
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
